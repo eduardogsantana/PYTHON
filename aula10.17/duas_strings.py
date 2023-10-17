cdeff67a0c3030a3ab85530fa825b95e3cb85801
Eduardo Gomes <zaikogomess@gmail.com>
2023-10-17 16:42:44 -0300

    adicionados mais comentarios ao duas_strings.py

--- aula10.17/duas_strings.py
+++ aula10.17/duas_strings.py
@@ -1,8 +1,11 @@
 # texto1 recebe o valor de "sou do"
-texto1 = "sou do"
+texto1 = '"sou do"'
 
 # texto2 recebe o valor "back-end"
-texto2 = "back-end"
+# caracter de SCAPE é o "\"
+# você pode ESCAPAR aspas duplas utilizando aspas simples
+# vale reforçar que isso só vale para aspas duplas, outros caracteres não
+texto2 = "\"back-end\""
 
 # a função print, concatena e imprime as duas variaveis na tela
 print(texto1, texto2)
