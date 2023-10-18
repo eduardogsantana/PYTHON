d7acc289f49e5ff1cc4190b221aeb41c03299ea2
Eduardo Gomes <zaikogomess@gmail.com>
2023-10-18 15:46:35 -0300

    adc o arq input_de_dados.py

---
+++ aula10.18/input_de_dados.py
@@ -0,0 +1,5 @@
+# para criar uma interação com o usuário, utilizamos a função "INPUT" 
+# 1. todo INPUT deve ter seu valor associado a uma variável
+
+nome = input('Digite seu nome aqui: ')
+print(nome)
