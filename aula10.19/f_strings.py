b0ae76c0e811a0919d9e92bb7b45c417e4fb52ef
Eduardo Gomes <zaikogomess@gmail.com>
2023-10-19 15:17:35 -0300

    Att arq f_strings.py

---
+++ aula10.19/f_strings.py
@@ -0,0 +1,9 @@
+# interpolando strings com codigo python
+
+nome = input("Digite seu nome:")
+
+#CONCATENAÇÃO
+print("Seja bem vindo, ",nome)
+
+#INTERPOLAÇÃO
+print(f'Seja muito bem-vindo {nome}. Estávamos esperando por você!!!')
