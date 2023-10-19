7c8f83bc4338745782cb8e03c04db2bfe8641346
Eduardo Gomes <zaikogomess@gmail.com>
2023-10-19 09:37:11 -0300

    arq desafio07.py add e funcionando

---
+++ aula10.18/desafio07.py
@@ -0,0 +1,6 @@
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+num3 = int(input('Digite só mais um número: '))
+
+print("Resultado 1: ",(2*num1)*(num2/2+num3))
+print("Resultado 2: ",((3*num1)+num3)*num2)
