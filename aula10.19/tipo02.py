92d141677a8890a082780ab2adb099504bdefb2d
Eduardo Gomes <zaikogomess@gmail.com>
2023-10-19 15:34:57 -0300

    arq tipo02.py atualizado com conteudo.

--- aula10.19/tipo02.py
+++ aula10.19/tipo02.py
@@ -1,1 +1,13 @@
 #entender mudanças de tipos dentro do python.
+
+a = 50
+b = "50" # entre aspas ele aparece como string. Mas se colocar como str (50), n se faz necessario a aspas.
+
+
+
+
+
+
+
+
+#utilizando o float: moedas, temperatura, peso, medidas e outros casos parecidos.
