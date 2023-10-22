4c712c75c96e4e270a87414b1bf2cadca1bfda05
Eduardo Gomes <zaikogomess@gmail.com>
2023-10-22 09:21:03 -0300

    arq atividade10.19_2.py add e att

---
+++ aula10.19/atividade10.19_2.py
@@ -0,0 +1,22 @@
+# Receba a altura(h) de uma pessoa, crie um código que calcule seu peso ideal, utilizando as fórmulas abaixo:
+# Para homens: (72.7*h) - 58
+# Para mulheres: (62.1*h) - 44.7
+
+print('Calculo do peso ideal do homem')
+
+altura1 = float(input('Digite a altura do homem:'))
+
+valor1 = (altura1 * 72.7) - 58
+
+
+print('Peso ideal do homem:', valor1)
+
+
+
+print('Calculo do peso ideal da mulher')
+
+altura1 = float(input('Digite a altura da mulher:'))
+
+valor1 = (altura1 * 62.1) - 44.7
+
+print('Peso ideal da mulher:', valor1)
