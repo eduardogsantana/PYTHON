e13ba3851c5e3701be290fb283ccfa2a03e7d4e9
Eduardo Gomes <zaikogomess@gmail.com>
2023-11-06 16:35:02 -0300

    Criado a pasta aula11.06 com conteúdo.

---
+++ aula11.06/ambiente_virtual/variaveis.py
@@ -0,0 +1,12 @@
+# variáveis são usadas para salvar algo na memoria do computador.
+# o sinal de igualdade(=) é utilizado para atribuição de valores.
+var01 = "Alguma coisa"
+print(var01)
+var01 = "Outra coisa"
+print(var01)
+var01 = 10
+print(var01)
+var01 = "A" * 10
+print(var01)
+var01 = "A" + "B" + "C"
+print(var01)
