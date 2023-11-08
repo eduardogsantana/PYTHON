fda3c499b5bd0d92fb658e64c8b4b3584de7157c
Eduardo Gomes <zaikogomess@gmail.com>
2023-11-08 14:35:28 -0300

    arquivo aula11.08 criado e att com aula01.py

---
+++ aula11.08/ambiente_virtual/aula01.py
@@ -0,0 +1,9 @@
+nome = "Eduardo Gomes"
+altura = 1.78
+peso = 100
+imc = peso / (altura * altura)
+
+print(f'{nome} tem {altura:.2f} de altura, pesa {peso} quilos e seu IMC é: ')
+print(f' {imc:.2f}')
+
+
