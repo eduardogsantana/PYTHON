50a50cce591926962a358b77cdb8b33ef9c9ba24
Eduardo Gomes <zaikogomess@gmail.com>
2023-11-10 09:20:20 -0300

    arq aula11.09 add

---
+++ aula11.09/aula01.py
@@ -0,0 +1,24 @@
+texto = "Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhado entre participantes, instrutores e empresas parceiras"
+
+print(texto[0:5:2])
+print(texto[5:15:2])
+print(texto[6:19:2])
+print(texto[12:23:2])
+print(texto[17:29:2])
+print(texto[19:29:2])
+print(texto[13:20:2])
+print(texto[17:27:3])
+print(texto[31:49:3])
+print(texto[102:130:4])
+print(texto[-57:-70:2])
+print(texto[0:251:5])
+# Escola que cria conhecimento e informação entre pessoas.
+palavra1 = texto[9:16]
+palavra2 = texto[110:113]
+palavra3 = texto[49:53]
+palavra4 = texto[76:88]
+palavra5 = texto[89:90]
+palavra6 = texto[33:42]
+print(palavra1,palavra2,palavra3,palavra4,palavra5,palavra6)
+
+#Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhado entre participantes, instrutores e empresas parceiras
