6696bf5db19fb183a7444d007fa3b082c92fa80f
Eduardo Gomes <zaikogomess@gmail.com>
2023-11-16 09:24:24 -0300

    atualização da aula11.14

--- aula11.14/atividade2.py
+++ aula11.14/atividade2.py
@@ -1,5 +1,5 @@
 # 2. faça um código que cria uma lista de 10 números reais e imprima só os pares e depois só os impares
 
 lista_2 = [1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.1]
-print(lista_2 [1:10:2])
-print(lista_2 [0:10:2])
+print(lista_2 [1::2])
+print(lista_2 [0::2])
--- aula11.14/atividade3.py
+++ aula11.14/atividade3.py
@@ -8,7 +8,6 @@ aluno1_3 = float(input("Digite uma nota: "))
 media1 = (aluno1_1 + aluno1_2 + aluno1_3) / 3
 
 lista_media.append(media1)
-print(lista_media)
 
 aluno2_1 = float(input("Digite uma nota: "))
 aluno2_2 = float(input("Digite uma nota: "))
@@ -17,7 +16,6 @@ aluno2_3 = float(input("Digite uma nota: "))
 media2 = (aluno2_1 + aluno2_2 + aluno2_3) / 3
 
 lista_media.append(media2)
-print(lista_media)
 
 aluno3_1 = float(input("Digite uma nota: "))
 aluno3_2 = float(input("Digite uma nota: "))
