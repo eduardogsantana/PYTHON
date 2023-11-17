ae483c2db8ce2f0759a439a6d4003d90b242f358
Eduardo Gomes <zaikogomess@gmail.com>
2023-11-17 11:06:50 -0300

    testes forca.py

--- Alura/Jogos/forca.py
+++ Alura/Jogos/forca.py
@@ -4,14 +4,27 @@ def jogar():
     print(34 * "*")
     print("Bem vindo ao jogo de Forca!!")
     print(34 * "*")
-    palavra_secreta = "batata"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
-    tentativas = 0  
 
+    arquivo = open ("palavras_forca.txt", "r")
+    palavras_1 = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras_1.append(linha)
+
+
+
+    arquivo.close()
+    print(palavras_1)
+
+    palavra_secreta = "batata".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+     
     print(letras_acertadas)
 
     enforcou = False
     acertou = False
+    tentativas = 0 
 
     #enquanto (True and True):
     while(not enforcou and not acertou):
@@ -29,12 +42,18 @@ def jogar():
         else:
             tentativas = tentativas + 1
 
-        enforcou = tentativas ==6
-
-    
-
+        enforcou = tentativas == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
+    if(acertou):
+        print("Você ganhou o jogo, PARABÉNS!!!!!")
+    else:
+        print("Você perdeu, tente novamente!!!")
+    
     print("!!!! Fim de jogo!!!!")
+
+def new_func():
+    return (read)
 
 if(__name__ == "__main__"):
     jogar()
