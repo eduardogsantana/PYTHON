7be0c9b18919a981ca9f0e362eea4f6016420df3
Eduardo Gomes <zaikogomess@gmail.com>
2023-11-20 13:41:41 -0300

    att teste.py orientação de objetos alura

--- Alura/Orientação de Objetos/teste.py
+++ Alura/Orientação de Objetos/teste.py
@@ -2,4 +2,8 @@ def criar_conta(numero, titular, saldo, limite):
     conta = {"Número": numero, "Titular": titular, "Saldo": saldo, "Limite": limite}
     return conta
 
+def desposita(conta, valor):
+    conta["Saldo"] += valor
 
+def saca(conta, valor):
+    conta["Saldo"] -= valor
