8d8dff75592d1a74d7b438e503b2adde3397fcc1
Eduardo Gomes <zaikogomess@gmail.com>
2023-12-12 16:30:57 -0300

    aula12.12

---
+++ aula12.12/orientacao_objetos.py
@@ -0,0 +1,32 @@
+# Entidade = Objeto
+# > Alguma coisa que tenha sua própria existência, características e que apresente alguma função dentro do mundo real.
+# > São abstrações dos objetos existentes do mundo real;
+# 	- Possuem os mesmos comportamentos;
+# 	- Possuem os mesmo estados;
+# > Podem representar os objetos no mundo real em vários níveis de abstração:
+# > Depende de decisões do observador do mundo.
+# > Sempre que criar uma classe, começas pelos atributos.
+
+# Objetos:
+# > Objetos possuem estados = (atributos, sempre serão ou terão relação com substantivos.) e comportamento = (métodos, sempre serão ou terão relação com verbos.).
+# > Uma porta tem:
+# 	° Estados: aberta, fechada;
+# 	° Comportamento: abrir, fechar;
+# 	° São instâncias de classes, com valores diferentes;
+# 	° Representa uma entidade do mundo real:
+# 	° Cada tem:
+# 	- Identidade
+# 	- Estado
+# 	- Comportamento
+
+# > E um carro:
+# 	° Que atributos e comportamentos ele tem?
+# 	- 
+
+# Atributos:
+# > Características que compõem um objeto:
+# 	° Pode ser um simples valor ou outro Objeto.
+# > Objeto: Funcionário
+# 	° Atributos: nome, cpf, etc.
+# > Objeto: Departamento
+# 	° Atributos: funcionário, localização.
