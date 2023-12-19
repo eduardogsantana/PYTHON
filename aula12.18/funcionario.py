346732b20f42bb613ae4e4487ccacf1160d0c8e9
Eduardo Gomes <zaikogomess@gmail.com>
2023-12-19 09:33:03 -0300

    funcionario.py iniciado

--- aula12.18/funcionario.py
+++ aula12.18/funcionario.py
@@ -1,15 +1,29 @@
 # cria a abstração para uma super classe funcionário com duas subclasses.
-
 class Funcionario:
     def __init__(self, nome, idade):
         self.nome = nome
-        self.nome = idade
+        self.idade = idade
 
-class recepcionista(Funcionario):
+class Professor(Funcionario):
     def __init__(self, nome, idade):
         super().__init__(nome, idade)
+        self.materia = input(f'Professor {nome}, digite qual matéria você ensina: ').capitalize()
+        self.turno = input(f'Professor {nome}, digite o seu turno: ').capitalize()
+    
+    def __str__(self):
+        return f'Seja bem-vindo, professor: {self.nome}, leciona a matéria: {self.materia}, no turno: {self.turno}'
         
 
-class vigia(Funcionario):
+class Diretor(Funcionario):
     def __init__(self, nome, idade):
         super().__init__(nome, idade)
+        self.direcao = input(f'Diretor {nome}, digite qual departamento você dirige: ').capitalize()
+
+    def __str__(self):
+        return f'Seja bem-vindo, diretor {self.nome}, você dirige o departamente: {self.direcao}'
+
+professor = Professor('Eduardo', 27)
+print(professor)
+
+diretor = Diretor('João', 27)
+print(diretor)
